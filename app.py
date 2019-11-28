4908c349719ab8f4bca261c9e47bd77e16d32785
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2019-11-28 13:51:28 +0100

    create dbreading.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from models.categorydownloader import CategoryDownloader
 from bdd.connection import Connection
 from bdd.dbcreate import DbCreate
 from bdd.dbadd  import DbAdd
+from bdd.dbreading import DbReading
 
 auth = Connection()
 auth.connect()
 
+"""
 create_db = DbCreate(auth)
 create_db.create_database()
 create_db.create_table()
@@ -17,7 +19,13 @@ category = CategoryDownloader()
 all_categories = category.get_category()
 
 add_db = DbAdd(auth)
+
 # Ajoute les categories a la bdd
 add_db.add_category(all_categories)
 add_db.add_product(all_categories)
-print()
+"""
+
+# Lecture de la bdd
+db_read = DbReading(auth)
+db_read.get_all_categories()
+db_read.get_category_products(4)
