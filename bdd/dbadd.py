d73326fe3c78729ea787ee7f374cf3e07b2b0136
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2019-12-03 18:53:57 +0100

    Modif add bdd

--- bdd/dbadd.py
+++ bdd/dbadd.py
@@ -32,13 +32,13 @@ class DbAdd:
                     cursor.execute(mySql_insert_query, (product.product_name_fr[:499], product.nutrition_grade_fr, product.id, product.brands, cat_id[0]))
                 except mysql.connector.errors.IntegrityError:
                     # Add logging here
-                    pass
+                    continue
 
-                for store in product.stores.split(","):
+                for store in product.stores.split(","):                    
                     query = "INSERT INTO openfood.store (name) VALUES (%s) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), name=name"                    
                     cursor.execute(query, (store.strip().lower(),))
-                    query = "INSERT INTO product_has_store (id_store, id_product) VALUES (LAST_INSERT_ID())"
-                    cursor.execute(query, (store.id_store, store.id_product,))
+                    query = "INSERT INTO openfood.product_has_store (id_store, id_product) VALUES (LAST_INSERT_ID(), %s)"
+                    cursor.execute(query, (product.id,))
                     
             
             self.connect.commit()
