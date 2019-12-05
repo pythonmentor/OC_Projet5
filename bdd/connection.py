44e2bacf66cdc914ad191c4a41ef140fae047e30
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2019-12-05 16:35:51 +0100

    Modif

--- bdd/connection.py
+++ bdd/connection.py
@@ -10,7 +10,7 @@ class Connection:
         self.host = 'localhost'
         self.database ='openfood'
         self.user = 'root'
-        self.password = ''
+        self.password = '#####'
 
     def connect(self):
         """Connection method"""
